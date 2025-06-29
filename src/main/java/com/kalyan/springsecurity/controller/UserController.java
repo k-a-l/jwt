c174455bb699e376a2189df83016ee7bb2908439
kalyan <kalyankatwal2@gmail.com>
2025-06-29 23:39:35 +0545

    Ready to use

--- src/main/java/com/kalyan/springsecurity/controller/UserController.java
+++ src/main/java/com/kalyan/springsecurity/controller/UserController.java
@@ -1,27 +1,53 @@
 package com.kalyan.springsecurity.controller;
 
+import com.kalyan.springsecurity.model.LoginResponse;
 import com.kalyan.springsecurity.model.User;
 import com.kalyan.springsecurity.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/user")
 public class UserController {
 private final UserRepository userRepository;
+private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/register")
-    public ResponseEntity<User> signp(@RequestBody User user) {
-        return ResponseEntity.ok(userRepository.save(user));
+    public ResponseEntity<?> signUp(@RequestBody User user) {
+        if (userRepository.existsUserByUsername(user.getUsername())) {
+            return ResponseEntity.badRequest().body(" Username already exists.");
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        // Optional: add a default role if not set
+        if (user.getRole() == null || user.getRole().isEmpty()) {
+            user.setRole(List.of("ROLE_USER"));
+        }
+
+        User savedUser = userRepository.save(user);
+        return ResponseEntity.ok(savedUser);
     }
 
-    @GetMapping("/open")
-    public String open() {
-        return "This is open";
+
+    @GetMapping("/all")
+    public ResponseEntity<List<LoginResponse>> getAll() {
+        List<LoginResponse> responses = userRepository.findAll().stream()
+                .map(user -> LoginResponse.builder()
+                        .username(user.getUsername())
+                        .role(user.getRole().toString())
+
+                        .build())
+                .toList();
+
+        return ResponseEntity.ok(responses);
     }
 
 }
